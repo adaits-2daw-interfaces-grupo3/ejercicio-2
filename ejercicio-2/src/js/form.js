5063b18b780248789e3b982c8b79844885597cd3
Trafalgar018 <eloyneolo@icloud.com>
2017-11-19 20:28:34 +0100

    Add files via upload

---
+++ ejercicio-2/src/js/form.js
@@ -0,0 +1,12 @@
+function muestra_oculta(id){
+    if (document.getElementById){ //se obtiene el id
+        var form = document.getElementById(id); //se define la variable "form" igual a nuestro div
+        form.style.display = (form.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+    }
+}
+/*
+Hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente
+ */
+window.onload = function(){
+    muestra_oculta('formulario');
+}
